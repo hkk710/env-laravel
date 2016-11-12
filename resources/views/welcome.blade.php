628a913a18a4430044a11c2534860e29eaf82457
hkk710 <hkk710@gmail.com>
2016-11-12 14:35:18 +0530

    bargraph added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@ body, html {
 }
 
 /* Create a Parallax Effect */
-.bgimg-1, .bgimg-2, .bgimg-3, .bgimg-4, .bgimg-5 {
+.bgimg-1, .bgimg-2, .bgimg-3, .bgimg-4, .bgimg-5, .bgimg-6 {
     opacity: 0.7;
     background-attachment: fixed;
     background-position: center;
@@ -59,16 +59,120 @@ body, html {
     min-height: 400px;
 }
 
+/* sixth image (footprint)*/
+.bgimg-6 {
+    background-image: url('images/top10.jpg');
+    min-height: 400px;
+}
+
 .w3-wide {letter-spacing: 10px;}
 .w3-hover-opacity {cursor: pointer;}
 
 /* Turn off parallax scrolling for tablets and phones */
 @media only screen and (max-width: 1024px) {
-    .bgimg-1, .bgimg-2, .bgimg-3, .bgimg-4, .bgimg-5 {
+    .bgimg-1, .bgimg-2, .bgimg-3, .bgimg-4, .bgimg-5, .bgimg-6 {
         background-attachment: scroll;
     }
 }
 
+/*bar chart*/
+#chart{
+  position:relative;
+  margin-top:40px;
+  width:500px;
+  height:200px;
+}
+@-webkit-keyframes bargrowth{
+  0%   {max-height: 0%;}
+  100%  {max-height: 100%;}
+}
+@keyframes bargrowth{
+  0%   {max-height: 0%;}
+  100%  {max-height: 100%;}
+}
+.bar{
+  -webkit-animation: bargrowth 9000ms ease;
+  animation: bargrowth 9000ms ease;
+  position:absolute;
+  bottom:0;
+  display:inline-block;
+  background: #89b800;
+  box-shadow: 1px -1px #79a300, 2px -2px #79a300, 3px -3px #79a300,   4px -4px #79a300, 5px -5px #79a300, 6px -6px #79a300, 7px -7px   #79a300, 8px -8px #79a300, 9px -9px #79a300, 10px -10px #79a300;
+}
+
+
+.wrapper{
+  text-align:center;
+  margin:0 auto;
+  margin-top:30px;
+  width:500px;
+  font-family:Helvetica;
+  font-size:12px;
+}
+.wrapper > h1, .wrapper > p{
+  margin:0;
+}
+
+
+#chart1{
+  position:relative;
+  margin-top:40px;
+  width:500px;
+  height:200px;
+}
+@-webkit-keyframes bargrowth{
+  0%   {max-height: 0%;}
+  100%  {max-height: 100%;}
+}
+@keyframes bargrowth{
+  0%   {max-height: 0%;}
+  100%  {max-height: 100%;}
+}
+.bar1{
+  -webkit-animation: bargrowth 9000ms ease;
+  animation: bargrowth 9000ms ease;
+  position:absolute;
+  bottom:0;
+  display:inline-block;
+  background: #89b800;
+  box-shadow: 1px -1px #79a300, 2px -2px #79a300, 3px -3px #79a300,   4px -4px #79a300, 5px -5px #79a300, 6px -6px #79a300, 7px -7px   #79a300, 8px -8px #79a300, 9px -9px #79a300, 10px -10px #79a300;
+}
+
+
+.wrapper1{
+  text-align:center;
+  margin:0 auto;
+  margin-top:30px;
+  width:500px;
+  font-family:Helvetica;
+  font-size:12px;
+}
+.wrapper1 > h1, .wrapper1 > p{
+  margin:0;
+}
+
+
+
+
+.face {
+    position: absolute;
+}
+
+.data-y {
+    background: none!important;
+    font-size: 22px;
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: bold;
+    color: #FFF;
+    text-align: center;
+    text-shadow: 1px 1px #666;
+    -webkit-transform: translateZ(1px);
+    -moz-transform: translateZ(1px);
+}
+
+
+
+/*upto here*/
 
 
 element {
@@ -287,6 +391,58 @@ Waste Management</p>
     </div>
   </div>
 </div>
+
+<!-- remove if any problem in bar chart-->
+
+<!-- Second Parallax Image with Portfolio Text -->
+<div class="bgimg-6 w3-display-container">
+  <div class="w3-display-middle">
+    <span class="w3-xxlarge w3-text-light-grey w3-wide">TOP 10 COUNTRIES</span>
+  </div>
+</div>
+
+
+<!-- Container (Portfolio Section) -->
+<div style="background-color:#000;color:#fff!important;">
+<div id="topcountries" class="w3-content w3-container w3-padding-64">
+  <h3 class="w3-center w3-container">TOP 10 COUNTRIES</h3>
+  <p class="w3-container w3-center" style="background-color:#fff;color:#000!important"></p>
+ </div>
+<div class="row" style="background-color:#000;color:#fff!important">
+  <div class="col-sm-3 col-xs-50" style="background-color:#000;color:#fff!important">
+  <div class="wrapper">
+  <h2>PRODUCING MOST AMOUNT WASTE PER YEAR&nbsp; </h2>
+  <div id="chart">
+  <p class="face data-y"> 254 &nbsp; 207 &nbsp;52 &nbsp; &nbsp; 49 &nbsp; &nbsp; 35 &nbsp; &nbsp;32 &nbsp; &nbsp; 32 &nbsp; &nbsp; 30 &nbsp; &nbsp; 26 &nbsp; &nbsp; 26 </p>
+  <br>
+  <br>
+  <br>
+  </div>  
+  <p>US &nbsp; &nbsp; Russia &nbsp; &nbsp; Japan &nbsp; &nbsp; Germany &nbsp; &nbsp; UK &nbsp; &nbsp; Mexico &nbsp; &nbsp; France &nbsp; &nbsp; Italy &nbsp; &nbsp; &nbsp;&nbsp; Spain &nbsp; &nbsp; &nbsp; Turkey &nbsp;</p>
+  <br>
+  <p> Million Tons/Year</p>
+  <br>
+  <br>
+  <br>
+  <br>
+</div>
+</div>
+  <div class="col-sm-9 col-xs-50">
+  <div class="wrapper1 ">
+  <h2>&nbsp;MOST EFFICIENT WASTE MANAGEMENT COUNTRIES </h2>
+  <div id="chart1"></div>  
+  <br>
+  <br>
+  <br>
+  <br>
+</div>
+  </div>
+</div>
+</div>
+
+
+<!-- upto here bar chart-->
+
 
 
 <!-- Third Parallax Image with Portfolio Text -->
@@ -1166,6 +1322,53 @@ function myFunction() {
         navbar.className = navbar.className.replace(" w3-card-2 w3-animate-top w3-white", "");
     }
 }
+
+
+
+//bar chart
+// Add values to the values array and see what happens :)
+var values = [3418.57,2786.00,699.86,659.48,471.06,430.68,430.68,403.76,349.93,349.93];
+
+drawChart(values,"#chart",7) // You can adjust the margin between each bar by changing 10 to whatever you like
+
+function drawChart(data,selector,padding){
+  var max = Math.max.apply(Math, data);
+  var chart = document.querySelector(selector);
+  var barwidth = ((chart.offsetWidth-(values.length-1)*padding-(data.length)*10)/data.length);
+  var sum = data.reduce(function(pv, cv) { return pv + cv; }, 0);
+  var left = 0;
+  for (var i in data){
+    var newbar = document.createElement('div');
+    newbar.setAttribute("class", "bar");
+    newbar.style.width=barwidth+"px";
+    newbar.style.height=((data[i]/max)*100)+"%";
+    newbar.style.left=left+"px";
+    chart.appendChild(newbar);
+    left += (barwidth+padding+10);
+  }
+}
+
+var values1 = [1000,10000,600,100,200,500,400,800,300,120];
+
+drawChart(values1,"#chart1",10) // You can adjust the margin between each bar by changing 10 to whatever you like
+
+function drawChart1(data,selector,padding){
+  var max1 = Math.max.apply(Math, data);
+  var chart1 = document.querySelector(selector);
+  var barwidth1 = ((chart.offsetWidth-(values.length-1)*padding-(data.length)*10)/data.length);
+  var sum1 = data.reduce(function(pv, cv) { return pv + cv; }, 0);
+  var left1 = 0;
+  for (var i in data){
+    var newbar1 = document.createElement('div');
+    newbar1.setAttribute("class", "bar1");
+    newbar1.style.width=barwidth+"px";
+    newbar1.style.height=((data[i]/max)*100)+"%";
+    newbar1.style.left=left+"px";
+    chart1.appendChild(newbar1);
+    left1 += (barwidth+padding+10);
+  }
+}
+//upto here
 </script>
 </body>
 </html>
